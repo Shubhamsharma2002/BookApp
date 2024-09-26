755f6d874f0731b7885feeaf71ccbbef09654c00
shubham sharma <shubhamtech004@gmail.com>
2024-09-27 03:17:42 +0530

    login completed with backend api

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../Services/auth.service';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  fb = inject(FormBuilder)
+  authService = inject(AuthService)
+  router = inject(Router)
+  loginForm !: FormGroup
 
+
+  ngOnInit():void{
+    this.loginForm = this.fb.group({
+      email:['', Validators.compose([Validators.required,Validators.email])],
+      password:['', Validators.required],
+    },
+  )  
+   }
+
+  login(){
+    this.authService.loginService(this.loginForm.value)
+    .subscribe({
+      next:(res)=>{
+        alert("user loged in");
+        this.router.navigate(["home"])
+        this.loginForm.reset();
+      },
+      error:(err)=>{
+        console.log(err);
+      }
+    })
+  }
 }
--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -10,4 +10,8 @@ export class AuthService {
   registerService(registerObj:any){
     return this.http.post<any>(`${apiUrls.authServiceApi}register`,registerObj);
   }
+
+  loginService(loginObj:any){
+    return this.http.post<any>(`${apiUrls.authServiceApi}login`,loginObj);
+  }
 }
