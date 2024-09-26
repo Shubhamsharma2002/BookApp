d7552e184c903d6d11affce6140ab008d12ad135
shubham sharma <shubhamtech004@gmail.com>
2024-09-26 21:02:17 +0530

    Router done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Pages/login/login.component';
+import { RegisterComponent } from './Pages/register/register.component';
+import { ResetComponent } from './Pages/reset/reset.component';
+import { ForgetPasswordComponent } from './Pages/forget-password/forget-password.component';
+import { HomeComponent } from './Pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'login',
+        component:LoginComponent
+    },
+    {
+        path:'register',
+        component:RegisterComponent
+    },
+    {
+        path:'forget-password',
+        component:ForgetPasswordComponent
+    },
+    {
+        path:'home',
+        component:HomeComponent
+    },
+];
